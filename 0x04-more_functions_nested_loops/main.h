ea211c732589e12f374e39268ebf695463394f10
Ekwueme-Optimal <chinenyecharity10.com>
2022-10-20 06:33:15 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,28 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks for specific char
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
 #endif
