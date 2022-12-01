1b96dc92e318fded00d75eb8209743cd5491f551
Ekwueme-Optimal <chinenyecharity10@gmail.com>
2022-12-01 08:35:29 +0000

    Task 0. 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+unsigned int binary_to_uint(const char *b);
+
+#endif
